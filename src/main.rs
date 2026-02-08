a04716d39b536dabdaf51f7d7ac65fc6bc75937d
Shiyi Yin <yinshiyi2020@gmail.com>
2026-02-08 10:46:55 -0800

    next version

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use rust_htslib::{bam, bam::Read};
 
 fn main() {
-    let mut bam = bam::Reader::from_path("test/test.bam").unwrap();
-    for r in bam.records() {
-        println!("{}", r.qname());
-    }
-    let bam = bam::Reader::from_path(&"test/test.bam").unwrap();
+    // let mut bam = bam::Reader::from_path("test_files/NA06984_T.bam").unwrap();
+    // for r in bam.records() {
+    //     println!("{:?}", r.unwrap().qname());
+    // }
+    let bam = bam::Reader::from_path(&"test_files/NA06984_T.bam").unwrap();
     let header = bam::Header::from_template(bam.header());
 
     // print header records to the terminal, akin to samtool
